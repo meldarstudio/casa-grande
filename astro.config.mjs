2a25bdb9ea925c7ac7b285783d3ea38ffa87e570
kevinzunigacuellar <kevinzunigacuellar@gmail.com>
2025-01-27 12:59:26 -0500

    feat(styles): add global styles and integrate Tailwind CSS refactor(components): update class names for consistency and improve layout chore(deps): update Tailwind CSS and Astro dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.casagrandevt.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
